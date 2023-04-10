1cf687ec277a38055caea8c5c358fe5c8d557969
William <sofiro4@gmail.com>
2023-04-09 22:49:41 -0400

    ADD: Agregar metodos contains y delete

--- src/Listas-enlazadas/LinkedList.js
+++ src/Listas-enlazadas/LinkedList.js
@@ -1,13 +1,14 @@
 import LinkedListNode from "./LinkedListNode.js";
 /**
- * Crear una instacia de la clase LinkedList
- * 
+ * @class LinkedList
+ * Crea una instacia de la clase LinkedList
+ * @property {LinkedListNode} head - Referencia a la cabeza de la lista
+ * @property {LinkedListNode} tail - Referencia a la cola de la lista
  */
 export default class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-    
   }
 
   /**
@@ -41,4 +42,47 @@ export default class LinkedList {
     }
     return this;
   }
+
+  /**
+   * Verifica si alguno de los nodos posee el valor ingresado como parametro
+   * @param {boolean} value - Valor del nodo a buscar en la lista enlazada
+   * @returns 
+   */
+  contains(value){
+    let node = head;
+    while (node && node.value !== value){
+      node = node.next;
+    }
+    if(!node) return false;
+    return true;
+  }
+
+  delete(value){
+    if(!this.head) return false;
+    let node = this.head;
+    if(node.value === value){
+      if(this.head === this.tail){
+        this.head = null;
+        this.tail = null;
+      } else{
+        this.head = this.head.next;
+        return true;
+      }
+    }
+    while(node.next && node.next.value !== value){
+      node = node.next;
+    }
+    if(!node.next){
+      if(node.next === this.tail){
+        this.tail = node;
+        this.tail.next = null;
+      } else{
+        node.next = node.next.next;
+      }
+      return true;
+    }
+    return false;
+  }
+
+
 }
